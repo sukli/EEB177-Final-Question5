6d40bc700780203aa57aa47fd4d85fdb84e0696e
Suk Li <syli312@ucla.edu>
2015-03-19 10:29:55 -0700

    first print function

--- problem5.py
+++ problem5.py
@@ -1,13 +1,27 @@
 #! /usr/bin/python
 
+# read the csv file into a dictionary data structure
 def read_csv_file_to_dic(filename):
+    # open file and split into lines
     f = open(filename, "r")
     lines = f.read().splitlines()
     dic = {}
-    for line in lines:
+    # iterate over lines, skipping over the first ('family,count')
+    for line in lines[1:]:
+        # split by comma and insert as k,v pair
         split = line.split(",")
-        dic[split[0]] = split[1]
-    return dic 
+        dic[split[0]] = int(split[1])
+    return dic
+
+def print_families(dic, species_threshold):
+    max_species_num = max(dic.values())
+    # prints all families if there exists at least one family with more species than species_threshold
+    if (species_threshold < max_species_num):
+        for key in sorted(dic):
+            print "Family %s contains %d species" % (key, dic[key])
+    # prints if there does not exist at least one family with more species than species_threshold
+    elif (species_threshold > max_species_num):
+        print "No families have more than %d species" % species_threshold
 
 def main():
     dic = read_csv_file_to_dic("richness_fishbase_family.csv")
